1648e61248dae87cb20083ead0ddc4029ce0df19
somkiat <somkiat.p@gmail.com>
2015-10-02 12:10:11 +0700

    Add test case 2

--- TDDSwiftTests/EmployeeTest.swift
+++ TDDSwiftTests/EmployeeTest.swift
@@ -8,6 +8,15 @@ class EmployeeTest: XCTestCase {
         let dictionary = employee.asDictionary()
         
         XCTAssertEqual(dictionary["id"] as? Int, 1)
+        XCTAssertEqual(dictionary["firstname"] as? String, "Somkiat")
+    }
+    
+    func testCreateNewEmployeeWithId2() {
+        let employee = Employee(id: 2, firstname: "Up1")
+        let dictionary = employee.asDictionary()
+        
+        XCTAssertEqual(dictionary["id"] as? Int, 2)
+        XCTAssertEqual(dictionary["firstname"] as? String, "Up1")
     }
     
 }
